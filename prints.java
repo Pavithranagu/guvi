106cc7638ab99593f16437322cece0a5a966e4f8
Pavithranagu <pavi.rachu@gmail.com>
2017-06-06 10:22:32 +0530

    Create prints.java

---
+++ prints.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class prints
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+if(s1.equals("Hello"))
+{
+for(int i=0;i<5;i++)
+{
+System.out.println(s1);
+}}
+else{
+System.out.println("give valid input");}
+}}
