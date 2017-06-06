58724c155b941e4518b9c4bba1dcd50a6e497164
Pavithranagu <pavi.rachu@gmail.com>
2017-06-06 10:32:52 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class reverse
+{
+public static void main(String args[])
+{
+StringBuffer ss=new StringBuffer(args[0]);
+System.out.print(ss.reverse());
+}}
