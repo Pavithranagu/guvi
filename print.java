58cce7c3c794c503274ad25736a718b927d0535d
Pavithranagu <pavi.rachu@gmail.com>
2017-06-06 10:15:00 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class print
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+for(int i=0;i<5;i++)
+{
+System.out.println(s1);
+}
+}}
